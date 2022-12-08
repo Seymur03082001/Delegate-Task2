7e8ea1ac334ecbd7e9ccdbaf1e6188c4e2c2d659
ca r215.07 <ca.r215.07@ca.local>
2022-12-07 21:20:37 -0800

    first commit

---
+++ Delegate-task-2/Models/Exam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_task_2.Models
+{
+    internal class Exam
+    {
+        public string Student { get; set; }
+        public int Point { get; set; }
+        public string Subject { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Exam(string student, int point, string subject, DateTime startDate, DateTime endDate)
+        {
+            Student = student;
+            Point = point;
+            Subject = subject;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+    
+}
---
+++ Delegate-task-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using Delegate_task_2.Models;
+using System.Collections.Generic;
+
+namespace Delegate_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Exam> exam = new List<Exam>();
+
+            DateTime StartDate = new DateTime(2022,12,7,12,00,00);
+            DateTime EndDate = new DateTime(2022,12,13,12,30,00);
+
+            DateTime StartDate1 = new DateTime(2022, 12, 7, 15, 00, 00);
+            DateTime EndDate1 = new DateTime(2022, 12, 13, 15, 30, 00);
+
+            Exam student1 = new Exam("Seymur Ibrahimov", 50, "Geography", StartDate, EndDate);
+            Exam student2 = new Exam("Ilham Quliyev", 50, "Kimya", StartDate, EndDate);
+            exam.Add(student1);
+            exam.Add(student2);
+
+            Console.WriteLine("exam list");
+            foreach (Exam item in exam.FindAll(exam => exam.Point > 50))
+            {
+                Console.WriteLine($"Student:{ item.Student} Point: {item.Point} Subject : {item.Subject} StartDate : {item.StartDate} EndDate :{item.EndDate}");
+            }
+           
+            foreach(Exam item in exam.FindAll(exam=> (exam.EndDate.Day - exam.StartDate.Day)<7))
+            {
+                Console.WriteLine($"Student:{item.Student} Point: {item.Point} Subject : {item.Subject} StartDate : {item.StartDate} EndDate :{item.EndDate}");
+            }
+            foreach(Exam item in exam.FindAll(exam => (exam.StartDate.Day - exam.EndDate.Day) > 1))
+            {
+                Console.WriteLine($"Student:{item.Student} Point: {item.Point} Subject : {item.Subject} StartDate : {item.StartDate} EndDate :{item.EndDate}");
+            }
+        }
+        
+    }
+}
